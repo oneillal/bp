e9dfb692aeddeee4de6349084808318c442eef3a
Alan O Neill <X00159362@myTUDublin.ie>
2019-11-12 13:41:58 +0000

    Added MSTest UnitTest project and some tests

---
+++ BPCalculator-UnitTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BPCalculator;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BloodPressureCategoryUnitTests
+    {
+        [TestMethod]
+        public void bpCategoryLowTest()
+        {
+            BloodPressure bp = new BloodPressure
+            {
+                Systolic = 88,
+                Diastolic = 55
+            };
+            Assert.AreEqual(BPCategory.Low, bp.Category);
+        }
+        [TestMethod]
+        public void bpCategoryNormalTest()
+        {
+            BloodPressure bp = new BloodPressure
+            {
+                Systolic = 100,
+                Diastolic = 63
+            };
+            Assert.AreEqual(BPCategory.Normal, bp.Category);
+
+        }
+        [TestMethod]
+        public void bpCategoryPreHighTest()
+        {
+            BloodPressure bp = new BloodPressure
+            {
+                Systolic = 121,
+                Diastolic = 86
+            };
+            Assert.AreEqual(BPCategory.PreHigh, bp.Category);
+        }
+        [TestMethod]
+        public void bpCategoryHighTest()
+        {
+            BloodPressure bp = new BloodPressure
+            {
+                Systolic = 144,
+                Diastolic = 99
+            };
+            Assert.AreEqual(BPCategory.High, bp.Category);
+        }
+        //[TestMethod]
+        //public void bpCategoryNegativeTest()
+        //{
+        //    BloodPressure bp = new BloodPressure
+        //    {
+        //        Systolic = 99,
+        //        Diastolic = 39
+        //    };
+        //    Assert.AreEqual(BPCategory.High, bp.Category);
+        //}
+    }
+}
